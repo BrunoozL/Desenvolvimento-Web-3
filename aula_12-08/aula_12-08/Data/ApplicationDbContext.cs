ecfbc024b69f7f9b48a89624fd144468b46cbe15
Bruno Righi <brunorighi267@gmail.com>
2025-08-19 09:42:06 -0300

    first commit

---
+++ aula_12-08/aula_12-08/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using aula_12_08.Models;
+
+namespace aula_12_08.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ aula_12-08/aula_12-08/Models/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace aula_12_08.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }
+        public double Preco { get; set; }
+    }
+}
